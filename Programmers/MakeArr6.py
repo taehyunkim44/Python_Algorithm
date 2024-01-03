3c094fbc73bf2496ef7d4932ff2e638f0b0bd491
김태현 <eiiyy12@nate.com>
2024-01-03 18:09:47 +0900

    프로그래머스 lv.0

---
+++ Programmers/MakeArr6.py
@@ -0,0 +1,19 @@
+// 프로그래머스 배열 만들기6
+def solution(arr):
+    stk = []
+    
+    for i in range(len(arr)):
+        if len(stk) == 0:
+            stk.append(arr[i])
+        else:
+            if stk[-1] == arr[i]:
+                stk.pop()
+                i += 1
+            elif stk[-1] != arr[i]:
+                stk.append(arr[i])
+                i += 1
+    
+    if len(stk) == 0:
+        return [-1]
+    
+    return stk
