b9acffa1082e7a1615e8dbbe4a4118acce4c2705
taehyun <eiiyy12@nate.com>
2024-01-05 21:32:47 +0900

    프로그래머스 lv.0

---
+++ Programmers/MakeSquare.py
@@ -0,0 +1,16 @@
+// 프로그래머스 정사각형 만들기
+
+def solution(arr):
+    row = len(arr)
+    col = len(arr[0])
+
+    max_size = max(row, col)
+
+    for i in range(row):
+        arr[i] += [0] * (max_size - col)
+    
+    if row < col:
+        for _ in range(col - row):
+            arr.append([0] * max_size)
+    
+    return arr
